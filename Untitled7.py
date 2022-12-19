e0009961a90d6f56475b04f32703064911b53c64
Charlie0603 <95859907+Charlie0603@users.noreply.github.com>
2022-12-19 20:45:58 +0800

    使用 Colaboratory 建立

---
+++ Untitled7.py
@@ -0,0 +1,47 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMLFR9sWLv/iVFBiQaVhVO5",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Charlie0603/123.py/blob/main/Untitled7.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "NnLkABjYaPRO"
+      },
+      "outputs": [],
+      "source": [
+        "import streamlit as st\n",
+        "\n",
+        "st.success(\"You did it !\")\n",
+        "st.error(\"Error\")\n",
+        "st.warnig(\"Warning\")\n",
+        "st.info(\"It's easy to build a streamlit app\")\n",
+        "st.exception(RuntimeError(\"RuntimeError exception\"))"
+      ]
+    }
+  ]
+}
