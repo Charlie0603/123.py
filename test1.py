575562e6c828cff9e0699b04362100e963cb2c34
Charlie0603 <95859907+Charlie0603@users.noreply.github.com>
2022-12-20 22:09:59 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,30 +2,36 @@ import streamlit as st
 st.title ("2022年最新發票兌獎程式")
 st.image("最新發票號碼.jpg")
 num=st.text_input('輸入您的發票號碼')
-ns = '05701942' 
-n1 = '97718570'                        
-n2 = ['88400675','73475574','53038222'] 
+ns = '11174120' 
+n1 = '59276913'                        
+n2 = ['18079736','20591738','64500205'] 
 if num == ns: st.write("對中 1000 萬元！")  
 if num == n1: st.write("對中 200 萬元！")
-# 頭獎判斷
+
 for i in n2:
     if num == i:
-        st.write("對中 20 萬元！")   
+        st.write("對中 20 萬元！") 
+        st.image("恭喜中獎.jpg")
         break
     if num[-7:] == i[-7:]:
-        st.write("對中 4 萬元！")   
+        st.write("對中 4 萬元！") 
+         st.image("恭喜中獎.jpg")
         break
     if num[-6:] == i[-6:]:
-        st.write("對中 1 萬元！")    
+        st.write("對中 1 萬元！") 
+         st.image("恭喜中獎.jpg")
         break
     if num[-5:] == i[-5:]:
-        st.write("對中 4000 元")    
+        st.write("對中 4000 元")  
+         st.image("恭喜中獎.jpg")
         break
     if num[-4:] == i[-4:]:
-        st.write("對中 1000 元！")   
+        st.write("對中 1000 元！")  
+         st.image("恭喜中獎.jpg")
         break
     if num[-3:] == i[-3:]:
-        st.write("對中 200 元！")    
+        st.write("對中 200 元！")  
+         st.image("恭喜中獎.jpg")
         break
     else:
       st.write("再接再厲下次加油")
