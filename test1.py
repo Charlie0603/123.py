1fff9574f332313efce06f415a032b21144c04ce
Charlie0603 <95859907+Charlie0603@users.noreply.github.com>
2022-12-20 22:14:01 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,9 +4,11 @@ st.image("最新發票號碼.jpg")
 num=st.text_input('輸入您的發票號碼')
 ns = '11174120' 
 n1 = '59276913'                        
-n2 = ['18079736','20591738','64500205'] 
-if num == ns: st.write("對中 1000 萬元！")  
+n2 = ['18079936','20591738','64500205'] 
+if num == ns: st.write("對中 1000 萬元！")
+    st.image("恭喜中獎.jpg")
 if num == n1: st.write("對中 200 萬元！")
+    st.image("恭喜中獎.jpg")
 
 for i in n2:
     if num == i:
