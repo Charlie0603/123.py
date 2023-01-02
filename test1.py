95c9e7f263481d93e8d599901d951096ad84aed8
Charlie0603 <95859907+Charlie0603@users.noreply.github.com>
2023-01-02 19:12:25 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -13,35 +13,35 @@ n2 = ['18079936','20591738','64500205']
 n3 = ['ns','n1','n2']
 if num == ns: st.success("對中 1000 萬元！")
 if num == ns: st.image("恭喜中獎.jpg")
-if num == n1: st.write("對中 200 萬元！")
+if num == n1: st.success("對中 200 萬元！")
 if num == n1: st.image("恭喜中獎.jpg")
-if num[-3:] == ns[-3:]: st.write("注意特別獎喔!")
-if num[-3:] == n1[-3:]: st.write("注意特獎喔!")
+if num[-3:] == ns[-3:]: st.warning("注意特別獎喔!") ＃顯示警告訊息
+if num[-3:] == n1[-3:]: st.warning("注意特獎喔!") ＃顯示警告訊息
 
 
 for i in n2:
 
     if num == i:
-        st.write("對中 20 萬元！") 
-        st.image("恭喜中獎.jpg")
+        st.success("對中 20 萬元！") ＃顯示成功訊息
+        st.image("恭喜中獎.jpg") 
         break
     elif num[-7:] == i[-7:]:
-        st.write("對中 4 萬元！") 
+        st.success("對中 4 萬元！") ＃顯示成功訊息
         st.image("恭喜中獎.jpg")
         break
     elif num[-6:] == i[-6:]:
-        st.write("對中 1 萬元！") 
+        st.success("對中 1 萬元！") ＃顯示成功訊息
         st.image("恭喜中獎.jpg")
         break
     elif num[-5:] == i[-5:]:
-        st.write("對中 4000 元")  
+        st.success("對中 4000 元")  ＃顯示成功訊息
         st.image("恭喜中獎.jpg")
         break
     elif num[-4:] == i[-4:]:
-        st.write("對中 1000 元！")  
+        st.success("對中 1000 元！")  ＃顯示成功訊息
         st.image("恭喜中獎.jpg")
         break
     elif num[-3:] == i[-3:]:
-        st.write("對中 200 元！")  
+        st.success("對中 200 元！")  ＃顯示成功訊息
         st.image("恭喜中獎.jpg")
         break
