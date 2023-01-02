3df6a3f384d622b1135a36fbbc656673d6db771a
Charlie0603 <95859907+Charlie0603@users.noreply.github.com>
2023-01-02 18:59:53 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 
 st.balloons()
-st.sidebar.image("台灣彩卷.jpg")
+st.sidebar.image("雲端發票.jpg")
 st.sidebar.title("電子發票指利用網際網路或其他電子方式，開立、傳輸或接收的統一發票。 它包含雲端發票及電子發票證明聯。")
 st.title ("2022年最新發票兌獎程式")
 st.image("最新發票號碼.jpg")
@@ -11,7 +11,7 @@ ns = '11174120'
 n1 = '59276913'                        
 n2 = ['18079936','20591738','64500205']
 n3 = ['ns','n1','n2']
-if num == ns: st.write("對中 1000 萬元！")
+if num == ns: st.success("對中 1000 萬元！")
 if num == ns: st.image("恭喜中獎.jpg")
 if num == n1: st.write("對中 200 萬元！")
 if num == n1: st.image("恭喜中獎.jpg")
